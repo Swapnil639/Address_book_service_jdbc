4d5ce9a66ddbc97ed1135d5bd7020442cbaaeece
Swapnil639 <swapnilpatil631997@gmail.com>
2022-10-14 16:43:30 +0530

    Added :Connected database to java program and retrieved all the data from database table

--- src/main/java/com/bridgelabz/AddressBookService.java
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
 
+import java.sql.*;
+
 public class AddressBookService {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Service");
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Status of connecting database to java code: ");
+        String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service";
+        String userName = "root";
+        String password = "Swapnil123";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded!");
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection done!!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM address_book");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstName") + " | " + resultSet.getString("lastName") + " | " + resultSet.getString("address") + " | " + resultSet.getString("city") + " | " + resultSet.getString("state") + " | " + resultSet.getInt("zip") + " | " + resultSet.getInt("phone_Number") + " | " + resultSet.getString("email_id") + " | " + resultSet.getString("bookName") + " | " + resultSet.getString("conatctType"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 }
